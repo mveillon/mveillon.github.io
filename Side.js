2e21ef588ad87a35fec0c176fc62b49189c813ad
mveillon <49701743+mveillon@users.noreply.github.com>
2019-04-29 21:03:09 -0700

    Add files via upload

--- Side.js
+++ Side.js
@@ -10,17 +10,17 @@ function setLink() {
 }
 
 function setLink2() {
-	let title = 'Not Just a Man';
-	let link = title.link('not-just-a-man.html');
+	let title = "It's ok to be scared of relationships. They're scary.";
+	let link = title.link('scared-of-relationships.html');
 	document.getElementById('link2').innerHTML = link;
 }
 
 function setPreview2() {
-	let text = 'Anthony, an idiot, wanted to get Chinese food with his girlfriend for lunch.';
+	let text = 'You know what? I’m going to start right off with a truly mind-blowing fact: ';
 	document.getElementById('preview2').innerHTML = text;
 }
 
 function setFooter() {
-	let text = "This blog belongs to Dominique Salman. Please don't steal it. This site was published with GitHub Pages. This site is under construction and not yet complete.";
+	let text = "This blog belongs to Dominique Salman. Please don't steal it. This blog is published using GitHub pages. This site is under construction and not yet complete.";
 	document.getElementById('footer').innerHTML = text;
 }
